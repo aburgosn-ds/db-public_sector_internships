1cdf5aa2f236bfa21f092d4d167679511c5207c0
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-06 11:33:31 -0500

    Set the logger

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILENAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_DIRNAME = LOG_FILENAME
+
+log_path = os.path.join(os.getcwd(), 'logs', LOG_DIRNAME)
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILENAME)
+
+
+logging.basicConfig(
+    # filename=LOG_FILE_PATH,
+    filename='testlog.log',
+    format="[ %(asctime)s ] %(name)s %(module)s %(filename)s %(lineno)d - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+
+logging.info('es un debug')

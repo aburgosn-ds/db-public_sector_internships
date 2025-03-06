4f82e423465a53306225927d690a40168804ef8c
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-06 13:59:59 -0500

    Create Custom Exception

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+def error_message_details(error, error_detail:sys):
+    '''
+    Customize the error message to be shown by Exception object
+    '''
+    _, _, exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = f"Error occured in python script name [{filename}] - line number [{exc_tb.tb_lineno}] - error message: {str(error)}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail:sys):
+        super().__init__(error)
+        self.error_message = error_message_details(error=error, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message

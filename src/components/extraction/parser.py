4a7232be9d2b8afe9904a6b05ba921ad6333cfb1
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-13 10:18:14 -0500

    Parser module

---
+++ src/components/extraction/parser.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+
+from src.exception import CustomException
+from src.logger import logger
+
+
+def get_soup(url):
+    '''
+    Gets the html of a web page and parses it. Returns soup object
+    '''
+    try:
+        response = requests.get(url)
+        content = response.text
+        soup = BeautifulSoup(content, 'html.parser')
+        
+        logger.info("HTML from URL parsed successfully")
+        
+        return soup
+    
+    except Exception as e:
+        raise CustomException(e, sys)

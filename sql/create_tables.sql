3b9931f0238d6915784e26df879b8b6a1fc6b7da
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-17 00:14:34 -0500

    Updating column contraints

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,19 +1,13 @@
-CREATE TABLE IF NOT EXISTS specific_locations (
-	`id` INT AUTO_INCREMENT,
-    `address` VARCHAR(200) UNIQUE,
-    PRIMARY KEY (`id`)
-);
-
 CREATE TABLE IF NOT EXISTS cities (
 	`id` INT AUTO_INCREMENT,
-    `name` VARCHAR(50) UNIQUE,
+    `name` VARCHAR(50) UNIQUE DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
 
 CREATE TABLE IF NOT EXISTS organizations (
 	`id` INT AUTO_INCREMENT,
-    `name` VARCHAR(200) UNIQUE,
+    `name` VARCHAR(200) UNIQUE DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -22,19 +16,18 @@ CREATE TABLE IF NOT EXISTS offers (
     `offer_page_code` INT UNSIGNED UNIQUE,
     `offer_title` VARCHAR(200) DEFAULT NULL,
     `vacants` TINYINT UNSIGNED DEFAULT NULL,
-    `type` ENUM('pre-profesional', 'profesional') DEFAULT NULL,
+    `type` ENUM('PRE-PROFESIONAL', 'PROFESIONAL') DEFAULT NULL,
     `to_apply` TEXT DEFAULT NULL,
     `organization_id` INT DEFAULT NULL,
     `specific_requirements` TEXT DEFAULT NULL,
     `knowledge` TEXT DEFAULT NULL,
     `salary` DECIMAL(6,2) DEFAULT NULL,
     `responsabilities` TEXT DEFAULT NULL,
-    `specific_location_id` INT DEFAULT NULL,
+    `specific_location` VARCHAR(200),
     `city_id` INT DEFAULT NULL, 
     `end_date` DATE DEFAULT NULL,
     `url` VARCHAR(200) NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`specific_location_id`) REFERENCES `specific_locations`(`id`),
     FOREIGN KEY (`city_id`) REFERENCES `cities`(`id`),
     FOREIGN KEY (`organization_id`) REFERENCES `organizations`(`id`)
 );

10073bf99155b76619c050fa9e0c32357b8a666c
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-17 00:16:12 -0500

    Deleting old specific_location table name

--- src/models/offers.py
+++ src/models/offers.py
@@ -17,10 +17,10 @@ class Offers:
         insert_rows(self.table_name, json)
 
     def insert_rows_dinamically(self, json):
-        insert_rows_dinamically(self.table_name, 
-                                columns=['specific_location_id', 'city_id', 'organization_id', 'careers_id'], 
-                                ref_tables_name=['specific_locations', 'cities', 'organizations', 'careers'], 
-                                ref_columns=[('specific_location', 'address'), ('city', 'name'), ('organization', 'name'), ('careers', 'name')], 
+        return insert_rows_dinamically(self.table_name, 
+                                columns=['city_id', 'organization_id', 'careers_id'], 
+                                ref_tables_name=['cities', 'organizations', 'careers'], 
+                                ref_columns=[('city', 'name'), ('organization', 'name'), ('careers', 'name')], 
                                 json=json,
                                 connection_table='careers_per_offer')
         

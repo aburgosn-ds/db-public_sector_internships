a12253d09596ed7295ef2b9f67df5f742df4a0b1
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-17 00:34:01 -0500

    Engine moved or deleted. Transaction engine.begin() moved for each dict_ in insert_rows_dinamically function. Loggers messages updated

--- src/database/insert_data.py
+++ src/database/insert_data.py
@@ -3,10 +3,10 @@ from sqlalchemy.engine import Connection
 
 from src.database.db_connection import get_connection
 from src.database.load_db_metadata import load_metadata
+from src.database.db_connection import get_connection
 from src.database.select_data import select_id
 from src.exception import CustomException
 from src.logger import logger
-from src.utils import multiply_dicts
 
 from typing import List, Dict, Optional
 
@@ -16,7 +16,8 @@ def insert_rows(table_name: str, json: List[Dict], one_by_one: bool = True, get_
     '''
     Inserts new rows into a table in the database.
     '''
-    metadata = load_metadata()
+    engine = get_connection()
+    metadata = load_metadata(engine)
     table = metadata.tables[table_name]
     
     inserted_ids = []
@@ -26,12 +27,11 @@ def insert_rows(table_name: str, json: List[Dict], one_by_one: bool = True, get_
         if connection:
             _execute_insert(table, json, one_by_one, get_pk, inserted_ids, connection)
         else:
-            engine = get_connection()   # Get engine
             with engine.begin() as connection:
                 _execute_insert(table, json, one_by_one, get_pk, inserted_ids, connection)
         
     
-        logger.info(f"Row(s) inserted succesfully in *{table_name}* table.")
+        logger.info(f"Row(s) inserted succesfully in *{table_name}* table. Inserted id(s): {inserted_ids}")
         return inserted_ids
     
     except Exception as e:
@@ -66,24 +66,23 @@ def insert_rows_dinamically(table_name:str, columns:List[str], ref_tables_name:L
 
     try:
         # Start a new connection with a starting transaction
-        with engine.begin() as connection:
-            # Json to be added into the main table
-            new_json = []
-            failed_offers = [] # Keep track of unsuccessful inserts
-            
-            # For each dict in json, appends new keys-values and insert them into the main table
-            for dict_ in json:
-                
+        failed_inserts = [] # Keep track of unsuccessful inserts
+        success_inserts = [] # Keep track of successful inserts
+        new_json = [] # Json to be added into the main table
+        
+        # For each dict in json, appends new keys-values and insert them into the main table
+        for dict_ in json:
+            with engine.begin() as connection:
                 # Capture and passes insert errors for each row
                 try:
                     new_dict_ = dict_.copy()
 
                     # For each referenced table performs the addition in the dict
                     for ref_table_name, ref_column, column in zip(ref_tables_name, ref_columns, columns):
-                        
+                            
                         ref_column_in_json = ref_column[0]
                         ref_column_in_ref = ref_column[1]
-                        
+                            
                         # Gets the id if exists the ref_colum value in the referenced table 
                         id = select_id(ref_table_name, ref_column_in_ref, new_dict_[ref_column_in_json])
 
@@ -109,21 +108,25 @@ def insert_rows_dinamically(table_name:str, columns:List[str], ref_tables_name:L
                                 new_dict_[column] = new_ids[0]
                             else:
                                 new_dict_[column] = new_ids
-                    
+                        
                     offer_id = insert_rows(table_name, [new_dict_], connection=connection)[0]
                     conn_json = [{'offer_id': offer_id, 'career_id': career_id} for career_id in new_dict_['careers_id']]
                     insert_rows(connection_table, conn_json, get_pk=False, connection=connection)
 
+                    success_inserts.append(offer_id)
+
                     # Adds the dictionary with keys-values added into the new json 
                     new_json.append(new_dict_)
-                    print(new_json)
+                    logger.info(f"Row inserted dynamically in all tables. Main table{table_name}, id: {offer_id}")
+                    print(f"Done. Id: {offer_id}")
 
                 except Exception as e:
-                    failed_offers.append({'offer_page_code': new_dict_['offer_page_code'], 'url': new_dict_['url']})
-                    logger.info(f"Error inserting into *{table_name}* table. Page code: {new_dict_['offer_page_code']}.")
+                    failed_inserts.append({'offer_page_code': new_dict_['offer_page_code'], 'url': new_dict_['url']})
+                    logger.info(f"Error inserting row into *{table_name}* table. Page code: {new_dict_['offer_page_code']}.")
+                    print(str(e))
 
-
-        logger.info(f"Data insered into *{table_name}({str(ref_tables_name)})*.")
+        logger.info(f"Data inserted successfully and dynamically into *{table_name}* table: {success_inserts}.")
 
     except Exception as e:
+        logger.info(f"Error: no data was able to insert.")
         raise CustomException(e, sys)

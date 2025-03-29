37c948be82f91552c5957c2bc125d560b029769e
NobodyPhy <104728439+NobodyPhy@users.noreply.github.com>
2025-03-28 22:13:50 -0500

    Add main script.

---
+++ main.py
@@ -0,0 +1,25 @@
+from src.components.extraction.extract import Extractor
+from src.components.transformation.nlp_processing import Processor
+
+from src.models.offers import Offers
+
+def main():
+
+    # 1. Extract hmtls and codes for all job offers
+    extractor = Extractor("https://www.convocatoriasdetrabajo.com/convocatorias-practicas-profesionales-preprofesionales-vigentes.php")
+    htmls, codes = extractor.extract_offers_htmls()
+
+    # 2. Filter codes that are not in the database
+    offers = Offers()
+    htmls_filtered = offers.filter_htmls(htmls, codes)
+
+    # 3. Process html and get the json
+    processor = Processor(htmls_filtered[0:2])
+    offer_details = processor.get_job_details()
+
+    # 4. Insert data
+    failed_offers = offers.insert_rows_dinamically(offer_details)
+    print(failed_offers)
+
+if __name__ == '__main__':
+    main()

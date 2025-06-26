a4f8d77ca9af4121b3d433efb00e61e9f0c2af0d
aburgosn-ds <104728439+aburgosn-ds@users.noreply.github.com>
2025-06-26 13:11:45 -0500

    Script to join all downloaded data into a single csv

--- notebooks/consolidate.py
+++ notebooks/consolidate.py
@@ -1,30 +1,32 @@
 import pandas as pd
-from src.database.db_connection import get_connection
-
-engine = get_connection()
-
-# Load database tables as dataframes
-offers = pd.read_sql_table('offers', con=engine)
-cities = pd.read_sql_table('cities', con=engine)
-organizations = pd.read_sql_table('organizations', con=engine)
-careers = pd.read_sql_table('careers', con=engine)
-careers_per_offer = pd.read_sql_table('careers_per_offer', con=engine)
-
-# Join tables to get a consolidate table
-careers_per_offer_careers = pd.merge(careers_per_offer, careers, how='left', left_on='career_id', right_on='id', validate='many_to_one', suffixes=['_a', '_b'])
-offers_careers = pd.merge(offers, careers_per_offer_careers, how='right', left_on='id', right_on='offer_id', validate='one_to_many',  suffixes=['_c', '_d'])
-offers_careers_cities = pd.merge(offers_careers, cities, how='left', left_on='city_id', right_on='id', validate='many_to_one', suffixes=['_e', '_f'])
-offers_all = pd.merge(offers_careers_cities, organizations, how='left', left_on='organization_id', right_on='id', validate='many_to_one', suffixes=['_g', '_h'])
-
-# Drop timestamps that came from all tables except that from offers table
-offers_all.drop(columns=['current_date_a', 'current_date_b', 'current_date_f', 'current_date'], inplace=True)
-
-# Drop primary keys, foreign keys have the same information
-offers_all.drop(columns=['id_c', 'id_d', 'id_g', 'id_h'], inplace=True)
-
-# Rename columns
-offers_all.rename({'current_date_e': 'current_date', 'name': 'organization', 'name_e': 'career', 'name_f': 'city'}, inplace=True)
 
 
-# Save table as csv
-offers_all.to_csv("notebooks/offers_data.csv", index=False)
+# Load tables data as dataframes
+offers = pd.read_csv('offers.csv')
+cities = pd.read_csv('cities.csv')
+organizations = pd.read_csv('organizations.csv')
+careers = pd.read_csv('careers.csv')
+careers_per_offer = pd.read_csv('careers_per_offer.csv')
+
+# Join careers with careers_per_offer and hen process
+careers_per_offer_careers = pd.merge(careers_per_offer, careers, how='left', left_on='career_id', right_on='id', validate='many_to_one')
+careers_per_offer_careers = pd.concat([careers_per_offer_careers.groupby('offer_id')['name'].agg(list), careers_per_offer_careers.groupby('offer_id')['career_id'].agg(list)], axis=1).reset_index()
+careers_per_offer_careers.rename({'name': 'careers', 'career_id': 'careers_id'}, inplace=True, axis=1)
+
+# Join previous df with offers and then process
+offers_careers = pd.merge(offers, careers_per_offer_careers, how='right', left_on='id', right_on='offer_id', validate='one_to_many')
+offers_careers.drop(columns='id', inplace=True)
+
+# Join previous df with cities and then process
+offers_careers_cities = pd.merge(offers_careers, cities, how='left', left_on='city_id', right_on='id', validate='many_to_one', suffixes=['', '_x'])
+offers_careers_cities.drop(columns=['current_date_x', 'id'], inplace=True)
+offers_careers_cities.rename({'name': 'city'}, inplace=True, axis=1)
+
+# Join previous df with organizations and then process
+offers_careers_cities_organizations = pd.merge(offers_careers_cities, organizations, how='left', left_on='organization_id', right_on='id', validate='many_to_one', suffixes=['', '_x'])
+offers_careers_cities_organizations.drop(columns=['current_date_x', 'id'], inplace=True)
+offers_careers_cities_organizations.rename({'name': 'organization'}, inplace=True, axis=1)
+
+
+# Save consolidate offers data
+offers_careers_cities_organizations.to_csv('consolidate.csv', index=False)
